1ac2385d619585842e4c04656f978aea080713fe
梅田実沙 <miltu@MacBook-Pro-2.local>
2026-04-25 22:34:48 +0900

    build for pages

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/my-portfolio-site",
+  assetPrefix: "/my-portfolio-site/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
