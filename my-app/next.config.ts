a57ee45b68efe0ec0a00a8a4b93d4acf1bfa3b73
梅田実沙 <miltu@MacBook-Pro-2.local>
2026-04-25 22:51:22 +0900

    switch from github pages to vercel

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,9 +1,7 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/my-portfolio-site",
-  assetPrefix: "/my-portfolio-site/",
   images: {
     unoptimized: true,
   },
